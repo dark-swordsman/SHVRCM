3f0c4a9423b973eebede5d780b17213cb23095f4
Kyle Barr <kylebarr54321@gmail.com>
2024-09-06 01:39:57 -0600

    added prisma, basic schema, ItemCard data pull

--- src/components/StoreItems/StoreItems.tsx
+++ src/components/StoreItems/StoreItems.tsx
@@ -1,25 +1,29 @@
+import prisma from "../../helper/db";
 import ItemCard from "./ItemCard";
 import StoreItemGrid from "./StoreItemGrid";
+import { Product } from "@prisma/client";
 
-export default function StoreItems() {
-  const items = [
-    { name: "TurboFlex 3000" },
-    { name: "AeroLight Pro" },
-    { name: "Quantum Shield" },
-    { name: "VitaBoost X1" },
-    { name: "ZenWave Ultra" },
-    { name: "PulseGrip" },
-    { name: "EcoSphere Elite" },
-    { name: "NeuraSync 2.0" },
-    { name: "HydroLift Max" }
-  ]
+export default async function StoreItems() {
+  // const items = [
+  //   { name: "TurboFlex 3000" },
+  //   { name: "AeroLight Pro" },
+  //   { name: "Quantum Shield" },
+  //   { name: "VitaBoost X1" },
+  //   { name: "ZenWave Ultra" },
+  //   { name: "PulseGrip" },
+  //   { name: "EcoSphere Elite" },
+  //   { name: "NeuraSync 2.0" },
+  //   { name: "HydroLift Max" }
+  // ]
+
+  const products: Product[] = await prisma.product.findMany();
 
   return (
     <div className="my-8">
       <h1 className="text-4xl font-semibold mb-6">Products</h1>
       <hr className="mb-6"/>
       <StoreItemGrid>
-        {items.map((item, i) => <ItemCard key={i} item={item} />)}
+        {products.map((product: Product, i) => <ItemCard key={product.id} product={product} />)}
       </StoreItemGrid>
     </div>
   );
